6287873e5457e76559eefd382ca9905ba5019368
lafee <jeansuh97@gmail.com>
2022-04-06 17:56:05 +0900

    Test: Save Password to DB using by BcryptPasswordEncoder

--- src/main/java/com/wisein/wiselab/controller/testController2.java
+++ src/main/java/com/wisein/wiselab/controller/testController2.java
@@ -3,6 +3,7 @@ package com.wisein.wiselab.controller;
 import com.wisein.wiselab.dto.MemberDTO;
 import com.wisein.wiselab.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 public class testController2 {
 
 	@Autowired
 	MemberService service;
+
+	@Autowired
+	BCryptPasswordEncoder passEncoder;
 
 	@GetMapping(value="/main")
 	public String main () throws Exception {
@@ -49,7 +56,28 @@ public class testController2 {
 
 	@PostMapping(value = "/reg")
 	public String postRegister (MemberDTO dto) throws Exception {
+
+		// Password Encoding by BCryptPasswordEncoder
+		String inputPw = dto.getPw();
+		String passEncd = passEncoder.encode(inputPw);
+		dto.setPw(passEncd);
+
 		service.register(dto);
+		return "redirect:/";
+	}
+
+
+	@PostMapping(value = "/login")
+	public String login(MemberDTO dto, HttpServletRequest req) throws Exception {
+
+		return "redirect:/";
+	}
+
+
+
+	@GetMapping(value = "/logout")
+	public String logout(HttpSession session) throws Exception {
+		session.invalidate();
 		return "redirect:/";
 	}
 
