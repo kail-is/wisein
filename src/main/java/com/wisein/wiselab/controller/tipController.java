83f31bc7654ef3a406d1e856c01f43947b4455fb
HANNAH <20133849@hanmail.net>
2022-05-22 21:08:59 +0900

    test3

---
+++ src/main/java/com/wisein/wiselab/controller/tipController.java
@@ -0,0 +1,73 @@
+package com.wisein.wiselab.controller;
+
+import com.wisein.wiselab.dto.QaListDTO;
+import com.wisein.wiselab.service.QaListService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Controller
+public class tipController {
+
+    @Autowired
+    QaListService qaListservice;
+
+    @GetMapping(value="/qalist")
+    public String qaList (Model model) throws Exception {
+
+        List<QaListDTO> qaList = new ArrayList<>();
+
+        qaList = (List<QaListDTO>) qaListservice.selectQaList();
+        if(qaList.size() > 0) {
+            for (int i = 0; i < qaList.size(); i++) {
+                System.out.println(i + " : " + qaList.get(i));
+            }
+        }
+
+        model.addAttribute("qaList", qaList);
+        return "cmn/qaList";
+    }
+
+    @GetMapping(value="/tipBoard")
+    public String qaBoard () throws Exception {
+        return "cmn/tipBoard";
+    }
+
+    @GetMapping(value="/regTipBoard")
+    public String regQaBoard (HttpServletRequest request, QaListDTO qaListDTO) throws Exception {
+
+        qaListDTO.setCategory("DB");
+        qaListDTO.setWriter("test2");
+        qaListDTO.setSubject(request.getParameter("title"));
+        qaListDTO.setContent(request.getParameter("content"));
+
+        System.out.println(qaListDTO.toString());
+
+        qaListservice.insertQaBoard(qaListDTO);
+
+        return "redirect:/tipList";
+    }
+
+    @GetMapping(value="/tipDetail")
+    public String qaDetail (HttpServletRequest request, QaListDTO qaListDTO, Model model) throws Exception {
+
+        System.out.println("qaNum : " + qaListDTO.getNum());
+
+        QaListDTO dto = qaListservice.selectQaOne(qaListDTO);
+
+        System.out.println(dto.toString());
+        System.out.println(dto.getContent());
+
+        model.addAttribute("qaListDTO", dto);
+        model.addAttribute("content", dto.getContent());
+
+        return "cmn/tipDetail";
+    }
+}
