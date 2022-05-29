303db81e9529f1c82cae10cc11e2907747d47ada
82109 <ghj0393@gmail.com>
2022-05-29 19:48:05 +0900

    qaBoard updata/delete

--- src/main/java/com/wisein/wiselab/controller/qaController.java
+++ src/main/java/com/wisein/wiselab/controller/qaController.java
@@ -2,17 +2,18 @@ package com.wisein.wiselab.controller;
 
 import com.wisein.wiselab.dto.QaListDTO;
 import com.wisein.wiselab.service.QaListService;
-import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,13 +30,15 @@ public class qaController {
         List<QaListDTO> qaList = new ArrayList<>();
 
         qaList = (List<QaListDTO>) qaListservice.selectQaList();
-        if(qaList.size() > 0) {
-            for (int i = 0; i < qaList.size(); i++) {
-                System.out.println(i + " : " + qaList.get(i));
-            }
-        }
+
+//        if(qaList.size() > 0) {
+//            for (int i = 0; i < qaList.size(); i++) {
+//                System.out.println(i + " : " + qaList.get(i));
+//            }
+//        }
 
         model.addAttribute("qaList", qaList);
+
         return "cmn/qaList";
     }
 
@@ -44,15 +47,14 @@ public class qaController {
         return "cmn/qaBoard";
     }
 
-    @GetMapping(value="/regQaBoard")
+    @PostMapping(value="/regQaBoard")
     public String regQaBoard (HttpServletRequest request, QaListDTO qaListDTO) throws Exception {
 
         qaListDTO.setCategory("DB");
         qaListDTO.setWriter("test2");
-        qaListDTO.setSubject(request.getParameter("title"));
-        qaListDTO.setContent(request.getParameter("content"));
 
-        System.out.println(qaListDTO.toString());
+
+//        System.out.println(qaListDTO.toString());
 
         qaListservice.insertQaBoard(qaListDTO);
 
@@ -60,18 +62,87 @@ public class qaController {
     }
 
     @GetMapping(value="/qaDetail")
-    public String qaDetail (HttpServletRequest request, QaListDTO qaListDTO, Model model) throws Exception {
+    public String qaDetail (HttpServletRequest request
+                            , QaListDTO dto
+                            , Model model
+                            , @RequestParam("num") int num) throws Exception {
 
-        System.out.println("qaNum : " + qaListDTO.getNum());
+        QaListDTO qaListDTO = null;
 
-        QaListDTO dto = qaListservice.selectQaOne(qaListDTO);
+        if (dto.getNum() != 0){
+//            System.out.println("/qaDetail qaListDTO.getNum() : " + qaListDTO.getNum());
+            qaListDTO = qaListservice.selectQaOne(dto);
+        } else {
+//            System.out.println("/qaDetail num : " + num);
+            qaListDTO.setNum(num);
+            qaListDTO = qaListservice.selectQaOne(dto);
+        }
 
-        System.out.println(dto.toString());
-        System.out.println(dto.getContent());
+//        System.out.println(qaListDTO.toString());
+//        System.out.println(qaListDTO.getContent());
 
-        model.addAttribute("qaListDTO", dto);
-        model.addAttribute("content", dto.getContent());
+        model.addAttribute("qaListDTO", qaListDTO);
+        model.addAttribute("content", qaListDTO.getContent());
 
         return "cmn/qaDetail";
     }
+
+    @GetMapping(value = "/qaDelete")
+    public String qaDelete (int num) throws Exception {
+
+//        System.out.println("num : " + num);
+
+        qaListservice.deleteQaBoard(num);
+
+        return "redirect:/qalist";
+    }
+
+    @GetMapping(value = "/qaUpdate")
+    public String qaUpdate (QaListDTO qaListDTO
+                            , Model model) throws Exception {
+
+//        System.out.println("/qaUpdate qaListDTO.getNum() : " + qaListDTO.getNum());
+
+        qaListDTO = qaListservice.selectQaOne(qaListDTO);
+
+//        System.out.println("/qaUpdate qaListDTO.toString() : " + qaListDTO.toString());
+//        System.out.println("qaListDTO.getContent() : " + qaListDTO.getContent());
+
+        model.addAttribute("qaListDTO", qaListDTO);
+//        model.addAttribute("content", qaListDTO.getContent());
+
+
+//        qaListservice.deleteQaBoard(num);
+
+        return "cmn/qaBoard";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/qaUpdatePro")
+    public void qaUpdatePro (QaListDTO qaListDTO) throws Exception {
+
+//        System.out.println("/qaUpdatePro qaListDTO : " + qaListDTO.toString());
+
+        qaListservice.updateQaBoard(qaListDTO);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
