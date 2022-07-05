c73b2262efb995c5625e8d6364d1c8d1b2fcfe4a
miocat <kkm1640@naver.com>
2022-07-05 14:37:17 +0900

    배포테스트

--- src/main/java/com/wisein/wiselab/ServletInitializer.java
+++ src/main/java/com/wisein/wiselab/ServletInitializer.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
--- src/main/java/com/wisein/wiselab/WiseinApplication.java
+++ src/main/java/com/wisein/wiselab/WiseinApplication.java
@@ -3,13 +3,15 @@ package com.wisein.wiselab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @ServletComponentScan
 @SpringBootApplication(exclude={MultipartAutoConfiguration.class})
-public class WiseinApplication {
+public class WiseinApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WiseinApplication.class, args);
@@ -22,4 +24,10 @@ public class WiseinApplication {
 		characterEncodingFilter.setForceEncoding(true);
 		return characterEncodingFilter;
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WiseinApplication.class);
+	}
+
 }
