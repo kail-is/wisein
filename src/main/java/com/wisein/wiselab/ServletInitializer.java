9cc7a059b9728050e7b005d4d72ce0143fc80122
miocat <kkm1640@naver.com>
2022-07-05 14:14:05 +0900

    배포테스트

--- src/main/java/com/wisein/wiselab/ServletInitializer.java
+++ src/main/java/com/wisein/wiselab/ServletInitializer.java
@@ -1,8 +1,10 @@
 package com.wisein.wiselab;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
