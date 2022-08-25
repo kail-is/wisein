0621cc62b3e62c919a9e20c0acc3f36d45afa3d8
jeansuh42 <jeansuh97@gmail.com>
2022-08-26 00:46:05 +0900

    ADD: H 1~3 태그 기준 사이드바 생성

--- src/main/webapp/resources/js/common/leftSideBar.js
+++ src/main/webapp/resources/js/common/leftSideBar.js
@@ -1,4 +1,4 @@
-function tagToList(tag,depth = 1,parentArr = []){
+function TreeTagToList(tag,depth = 1,parentArr = []){
     //초기화
     var exceptionMap = {'ul' : '예외zdzdazzz'}
 
@@ -14,6 +14,34 @@ function tagToList(tag,depth = 1,parentArr = []){
         }
         parentArr.push({'tagTitle' : tagTitle,'subTag':subArr, 'depth' : depth, 'id' : el.id})
         tagToList(subTag,depth + 1,subArr)
+    });
+
+    return parentArr;
+}
+
+
+function hTagToList(tag, depth = 1,parentArr = []){
+
+    var exceptionMap = {'' : ''}
+
+    if(!tag) return;
+
+    //태그 넣기
+    [...tag.childNodes].forEach((el,idx) => {
+        if(el.nodeName.startsWith("H")){
+            let subArr = [];
+            depth = el.nodeName.substring(el.nodeName.length-1)
+            let tagTitle = el.innerText
+
+            if(Object.keys(exceptionMap).indexOf(el.localName) > -1){//예외 태그
+                tagTitle = exceptionMap[el.localName];
+            }
+
+            if(depth <= 3){
+            // 1~3 태그만 등록
+                parentArr.push({'tagTitle' : tagTitle, 'depth' : depth, 'id' : el.id})
+            }
+        }
     });
 
     return parentArr;
@@ -36,4 +64,21 @@ function listToTag(target,arr,depth){
 
 }
 
-export {listToTag,tagToList} 
+
+function listToHTag(target,arr,depth){
+    //초기화
+    if(typeof target != 'object' || Array.isArray(target))
+        return;
+
+    depth = depth ? depth : 1
+
+    arr.forEach(function(e){
+        target.innerHTML +='<li style = "cursor: pointer; padding-left:' + (e.depth * 10) +'px"> <a href="#'+e.id+'">' +  e.tagTitle + ' </a></li>';
+        if(Array.isArray(e.subTag) && e.subTag.length > 0){
+            listToTag(target,e.subTag,depth + 1);
+        }
+    })
+
+}
+
+export {hTagToList, listToHTag}
